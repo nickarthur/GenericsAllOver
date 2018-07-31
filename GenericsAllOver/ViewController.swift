93ef8771d340d16eda0cf40c76e4c8faa86122b1
Stanislaw Kubis <sfqbis@gmail.com>
2018-07-31 22:47:35 +0200

    Initial Commit

---
+++ GenericsAllOver/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GenericsAllOver
+//
+//  Created by Stanisław Kubiś on 31.07.2018.
+//  Copyright © 2018 Stanisław Kubiś. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
